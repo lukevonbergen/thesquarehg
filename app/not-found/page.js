bf07302cd0d5d46d8b24fd48005e0fe6262661bb
lukevonbergen <57679649+lukevonbergen@users.noreply.github.com>
2025-06-25 12:44:23 +0000

    404

---
+++ app/not-found/page.js
@@ -0,0 +1,8 @@
+// app/not-found/page.jsx
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Page Not Found</h1>
+    </div>
+  );
+}
